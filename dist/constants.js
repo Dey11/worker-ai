f369f073cf7a7c2605fe8a057b87d8ccc58fe57f
Dey11 <shreyandey11@gmail.com>
2025-03-04 03:37:18 +0530

    update constant

--- dist/constants.js
+++ dist/constants.js
@@ -1,8 +1,8 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.MERGE_PDF_QUEUE_NAME = exports.QNA_QUEUE_NAME = exports.BUCKET_NAME = exports.QUEUE_NAME = void 0;
+exports.MERGE_PDF_QUEUE_NAME = exports.BUCKET_NAME = exports.QNA_QUEUE_NAME = exports.QUEUE_NAME = void 0;
 exports.QUEUE_NAME = "theoryQueue";
-exports.BUCKET_NAME = "test";
+// export const BUCKET_NAME = "test";
 exports.QNA_QUEUE_NAME = "qbankQueue";
-// export const BUCKET_NAME = "prod";
+exports.BUCKET_NAME = "prod";
 exports.MERGE_PDF_QUEUE_NAME = "mergePdfQueue";
