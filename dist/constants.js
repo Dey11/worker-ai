3cda7bcd5d56ec35aeb4a8afaf1bc1b663f4d96b
Dey11 <shreyandey11@gmail.com>
2025-03-08 15:24:09 +0530

    remove dist

--- dist/constants.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.MERGE_PDF_QUEUE_NAME = exports.BUCKET_NAME = exports.QNA_QUEUE_NAME = exports.QUEUE_NAME = void 0;
-exports.QUEUE_NAME = "theoryQueue";
-// export const BUCKET_NAME = "test";
-exports.QNA_QUEUE_NAME = "qbankQueue";
-exports.BUCKET_NAME = "prod";
-exports.MERGE_PDF_QUEUE_NAME = "mergePdfQueue";
